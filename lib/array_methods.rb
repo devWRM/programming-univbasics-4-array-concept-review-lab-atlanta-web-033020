3b8692b01352e808c4b3c369dd22b5b66398eb9b
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-02-26 21:02:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,7 +13,7 @@ end
 
 def find_max_value(array)
   # Add your solution here
-
+    maximum = array[0]
     array.length.times do |index|
         if array[index] >= array[0]
             maximum = array[index]
